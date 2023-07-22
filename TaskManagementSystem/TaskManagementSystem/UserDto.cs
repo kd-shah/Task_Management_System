61b6e3bfc192676bf9a3aaa9991516bcd7a1cbb5
Kunal Shah <kunal190410107121@gmail.com>
2023-07-22 15:53:46 +0530

    Register Feature

---
+++ TaskManagementSystem/TaskManagementSystem/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManagementSystem
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
